c4cded7703c0c08c446fbe29cbc3faca8d9b0719
meheraz1100 <meheraz1100@gmail.com>
2024-03-24 20:47:19 +0600

    post component added

---
+++ src/Post.jsx
@@ -0,0 +1,12 @@
+import './Post.css'
+
+export default function Post({post}){
+    return (
+        <div className='post'>
+            <h5>Title: </h5>
+            <p><small>User ID: </small></p>
+            <p><small>Post ID: </small></p>
+            <p></p>
+        </div>
+    )
+}
